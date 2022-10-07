d4f18d39d7790bedb252bdb9ca79b5b42e1d1f30
Sergey Rushev <rushev84@yandex.ru>
2022-10-07 11:52:49 +0300

    feat: add page for creating article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,4 +22,35 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+
+    // Вывод формы
+    public function create()
+    {
+        // Передаём в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса для извлечения данных
+    public function store(Request $request)
+    {
+        // Проверка введённых данных
+        // Если будут ошибки, то возникнет исключение
+        // Иначе возвращаются данные формы
+
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:50',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($data);
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут
+        return redirect()
+            ->route('articles.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,14 @@ Route::get('about', [PageController::class, 'about']);
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
 
+Route::get('articles/create', [ArticleController::class, 'create'])
+    ->name('articles.create');
+
+Route::post('articles/store', [ArticleController::class, 'store'])
+    ->name('articles.store');
+
 Route::get('articles/{id}', [ArticleController::class, 'show'])
     ->name('articles.show');
+
+Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
+    ->name('articles.edit');
