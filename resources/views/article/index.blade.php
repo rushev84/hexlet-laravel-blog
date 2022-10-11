005f0480b3e7b4d45d8a88a82e70558b2d538377
Sergey Rushev <rushev84@yandex.ru>
2022-10-11 11:05:55 +0300

    add destroy of the article

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,6 +4,7 @@
     <h1>Список статей</h1>
     @foreach ($articles as $article)
         <h2><a href="{{route('articles.show', $article->id)}}">{{$article->name}}</a></h2>
+        <a href="{{route('articles.destroy', $article->id)}}" data-method="delete" rel="nofollow">Удалить</a>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
 
 Route::patch('articles/{id}', [ArticleController::class, 'update'])
     ->name('articles.update');
+
+Route::delete('articles/{id}', [ArticleController::class, 'destroy'])
+    ->name('articles.destroy');
