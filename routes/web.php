233d9f1016c99f5c027d3dd77bf94eb0a10226ef
Sergey Rushev <rushev84@yandex.ru>
2022-10-12 09:17:04 +0300

    fix route articles.store

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('articles', [ArticleController::class, 'index'])
 Route::get('articles/create', [ArticleController::class, 'create'])
     ->name('articles.create');
 
-Route::post('articles/store', [ArticleController::class, 'store'])
+Route::post('articles', [ArticleController::class, 'store'])
     ->name('articles.store');
 
 Route::get('articles/{id}', [ArticleController::class, 'show'])
